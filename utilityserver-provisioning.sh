f10df628a0e9301ad60c3da8006ca27fac63f23e
orjanb314 <98532987+orjanb314@users.noreply.github.com>
2022-03-11 15:14:53 +0100

    Add files via upload

---
+++ utilityserver-provisioning.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+mkdir -p  /var/log/cloud /config/cloud /var/config/rest/downloads
+
+LOG_FILE=/var/log/cloud/startup-script.log
+[[ ! -f $LOG_FILE ]] && touch $LOG_FILE || { echo "Run Only Once. Exiting"; exit; }
+npipe=/tmp/$$.tmp
+trap "rm -f $npipe" EXIT
+mknod $npipe p
+tee <$npipe -a $LOG_FILE /dev/ttyS0 &
+exec 1>&-
+exec 1>$npipe
+exec 2>&1
+
+#: <<'END'
+yum install gcc make libffi-devel perl zlib-devel diffutils selinux-policy-devel -y
+wget --content-disposition https://dl.duosecurity.com/duoauthproxy-latest-src.tgz
+tar xzf duoauthproxy-*
+cd duoauthproxy-*
+make
+sudo ./duoauthproxy-build/install --install-dir /opt/duoauthproxy --service-user duo_authproxy_svc --log-group duo_authproxy_grp --create-init-script yes --enable-selinux=yes
+#END
+
+: <<'END'
+echo "Testinnhold" >> /home/orjan/testfil
+sudo apt-get install build-essential libffi-dev perl zlib1g-dev -y
+wget --content-disposition https://dl.duosecurity.com/duoauthproxy-latest-src.tgz
+tar xzf duoauthproxy-*
+duodir=$(ls -d */)
+cd $duodir
+make
+sudo ./duoauthproxy-build/install --install-dir /opt/duoauthproxy --service-user duo_authproxy_svc --log-group duo_authproxy_grp --create-init-script yes
+END
