28ef5040d166e2a260370454964509b45287ac31
orjanb314 <98532987+orjanb314@users.noreply.github.com>
2022-03-14 14:52:33 +0100

    Add files via upload

--- utilityserver-provisioning.sh
+++ utilityserver-provisioning.sh
@@ -19,3 +19,22 @@ duodir=$(ls -d */)
 cd $duodir
 make
 sudo ./duoauthproxy-build/install --install-dir /opt/duoauthproxy --service-user duo_authproxy_svc --log-group duo_authproxy_grp --create-init-script yes --enable-selinux=yes
+
+duo_ikey=$1
+duo_skey=$2
+duo_api_host=$3
+selfip_internal=$4
+bigip_radius_secret=$5
+
+echo $duo_intkey $duo_seckey $duo_apihost $selfip_internal $bigip_radius_secret
+
+cat << EOF >> /opt/duoauthproxy/conf/authproxy.cfg
+
+[radius_server_duo_only]
+ikey=$duo_ikey
+skey=$duo_skey
+api_host=$duo_api_host
+failmode=safe
+radius_ip_1=$selfip_internal
+radius_secret_1=$bigip_radius_secret
+EOF
