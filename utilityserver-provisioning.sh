96c5b93c58bf677b9661ce7eb87655c7ade1b19d
orjanb314 <98532987+orjanb314@users.noreply.github.com>
2022-03-14 15:06:17 +0100

    Add files via upload

--- utilityserver-provisioning.sh
+++ utilityserver-provisioning.sh
@@ -28,7 +28,42 @@ bigip_radius_secret=$5
 
 echo $duo_intkey $duo_seckey $duo_apihost $selfip_internal $bigip_radius_secret
 
-cat << EOF >> /opt/duoauthproxy/conf/authproxy.cfg
+cat << EOF > /opt/duoauthproxy/conf/authproxy.cfg
+; Complete documentation about the Duo Auth Proxy can be found here:
+; https://duo.com/docs/authproxy_reference
+
+; NOTE: After any changes are made to this file the Duo Authentication Proxy
+; must be restarted for those changes to take effect.
+
+; MAIN: Include this section to specify global configuration options.
+; Reference: https://duo.com/docs/authproxy_reference#main-section
+;[main]
+
+
+; CLIENTS: Include one or more of the following configuration sections.
+; To configure more than one client configuration of the same type, append a
+; number to the section name (e.g. [ad_client2])
+
+;[ad_client]
+;host=
+;service_account_username=
+;service_account_password=
+;search_dn=
+
+
+; SERVERS: Include one or more of the following configuration sections.
+; To configure more than one server configuration of the same type, append a
+; number to the section name (e.g. radius_server_auto1, radius_server_auto2)
+
+;[radius_server_auto]
+;ikey=
+;skey=
+;api_host=
+;radius_ip_1=
+;radius_secret_1=
+;failmode=safe
+;client=ad_client
+;port=1812
 
 [radius_server_duo_only]
 ikey=$duo_ikey
@@ -38,3 +73,5 @@ failmode=safe
 radius_ip_1=$selfip_internal
 radius_secret_1=$bigip_radius_secret
 EOF
+
+/opt/duoauthproxy/bin/authproxyctl start
