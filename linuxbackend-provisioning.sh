98df71c4dc8480f5a9a2ad4e684d7b5aa5e1be96
orjanb314 <98532987+orjanb314@users.noreply.github.com>
2022-03-21 12:09:59 +0100

    Add files via upload

---
+++ linuxbackend-provisioning.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+mkdir -p  /var/log/cloud /config/cloud /var/config/rest/downloads
+
+LOG_FILE=/var/log/cloud/startup-script.log
+[[ ! -f $LOG_FILE ]] && touch $LOG_FILE || { echo "Run Only Once. Exiting"; exit; }
+npipe=/tmp/$$.tmp
+trap "rm -f $npipe" EXIT
+mknod $npipe p
+tee <$npipe -a $LOG_FILE /dev/ttyS0 &
+exec 1>&-
+exec 1>$npipe
+exec 2>&1
+
+#Ubuntu
+#sudo apt update
+#sudo apt-get -y install apache2
+#hostname=$(hostname)
+#sudo sed -i "s/It works!/It works! ($hostname)/" /var/www/html/index.html
+#sudo service apache2 start
+
+#Centos
+sudo dnf install httpd
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --reload
+hostname=$(hostname)
+sudo sed -i "s/It works!/It works! ($hostname)/" /var/www/html/index.html
+sudo systemctl start httpd
